ae410ed2d483c610e3683f356ad7b41c006dd35f
Romadelf <romadelfatwork@hotmail.com>
2019-10-28 11:27:41 +0100

    Spring initializr done

---
+++ src/main/java/be/technifutur/jasa/rdf/TestSonarCloudDirtyProject/TestSonarCloudDirtyProjectApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.jasa.rdf.TestSonarCloudDirtyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSonarCloudDirtyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSonarCloudDirtyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/technifutur/jasa/rdf/TestSonarCloudDirtyProject/TestSonarCloudDirtyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.jasa.rdf.TestSonarCloudDirtyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSonarCloudDirtyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

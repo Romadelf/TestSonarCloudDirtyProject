893d77c3e9652914d9af93900f243803ad47ba21
Romadelf <romadelfatwork@hotmail.com>
2019-10-28 12:11:18 +0100

    Pseudo controler responding

---
+++ src/main/java/be/technifutur/jasa/rdf/TestSonarCloudDirtyProject/controller/ControllerTest.java
@@ -0,0 +1,16 @@
+package be.technifutur.jasa.rdf.TestSonarCloudDirtyProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+@Controller
+public class ControllerTest {
+
+    @GetMapping(value = "test", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> test() {
+	return ResponseEntity.ok("Ca marche");
+    }
+    
+}
